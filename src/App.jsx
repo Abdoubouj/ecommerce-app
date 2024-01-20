882641f30b9c58a0d740a734d975b1ee9ea1750c
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-20 20:53:37 +0100

    add cart component to App File

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import { Routes, Route } from "react-router-dom";
 import ProductsCategory from "./components/ProductsCategory/ProductsCategory";
 import SearchProducts from "./components/SearchProducts/SearchProducts";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
+import Footer from "./components/Footer/Footer";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
-    <div className="container-2xl bg-white min-h-screen px-6">
+    <div className="container-2xl bg-white min-h-screen">
       <Header />
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/products/search/:query" element={<SearchProducts />} />
         <Route path="/category/:category" element={<ProductsCategory/>}/>
         <Route path="/products/:id" element={<ProductDetails/>}/>
+        <Route path="/cart" element={<Cart/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
