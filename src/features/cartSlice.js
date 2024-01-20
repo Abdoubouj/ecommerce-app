ed460d30b7ea8561e4fed322464006465b5a1e77
AbdouBouj <abderrahimboujedar@gmail.com>
2024-01-20 20:58:19 +0100

    create CartSlice

---
+++ src/features/cartSlice.js
@@ -0,0 +1,28 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    cart:[],
+}
+const cartSlice = createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addToCart : (state,action)=>{
+            const item = state.cart.find((i)=> i.id === action.payload.id);
+            if(item){
+                state.cart = state.cart.map((i)=>{
+                    if(i.id === item.id){
+                        return {...i,quantity:i.quantity + 1}
+                    }
+                    return i;
+                })
+            }else{
+                state.cart.push(action.payload);
+            }
+        }
+    }
+})
+
+export const {addToCart} = cartSlice.actions;
+
+export default cartSlice.reducer;
